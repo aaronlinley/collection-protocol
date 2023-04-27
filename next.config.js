a6877ebe8c5c0d291e1df37a465d34c6a2381152
aaronlinley <linley.aaron@gmail.com>
2023-04-27 22:16:31 +0100

    Add most recent models

--- next.config.js
+++ next.config.js
@@ -9,6 +9,14 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'images.squarespace-cdn.com'
       },
+      {
+        protocol: 'https',
+        hostname: 'shop.asmodee.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'b2b.asmodee.es'
+      },
     ],
   }
 }
